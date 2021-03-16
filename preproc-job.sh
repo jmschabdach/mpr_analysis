2e3e02b544b740f6f41a4675c62caf51c64ad986
jmschabdach <jmschabdach@gmail.com>
2021-03-16 12:28:32 -0400

    adding preprocessing scripts from HPC

---
+++ preproc-job.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# The name of the job
+#$ -N reconall
+#
+# Join stdout and stderrL:
+#$ -j y
+#
+# Specify environment variables
+# -v SUBJECTS_DIR="/cbica/projects/bgdimagecentral/images/derivatives/freesurfer_7_1_0/"
+# -v FREESURFER_HOME="/cbica/software/external/freesurfer/centos7/7.1.0/"
+
+#module purge
+module unload freesurfer/5.3.0
+module load freesurfer/6.0.0
+
+# bash /cbica/software/external/freesurfer/centos7/7.1.0/SetUpFreeSurfer.sh
+
+source $FREESURFER_HOME/SetUpFreeSurfer.sh
+
+BASE=/cbica/projects/bgdimagecentral/mpr_analysis
+
+WORKINGDIR=$1
+IN=$2
+OUT=$3
+
+bash $BASE/preprocWashUACPCAlignment.sh --workingdir=$WORKINGDIR --in=$IN --out=$OUT --omat="premat.mat"
+
