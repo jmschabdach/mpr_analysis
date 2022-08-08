a06f2ce919078505dafe21fc4a6316ee65fb80ac
jmschabdach <jmschabdach@gmail.com>
2022-08-08 16:20:20 -0400

    set up the dataframes to be passed to the centilization tool

---
+++ r/prepare_data_for_centilization.r
@@ -0,0 +1,50 @@
+
+# fn <- '/Users/youngjm/Data/clip/tables/CLIPv0.7/2022-07-29_highres_nocontrast_singlescanpersubject.csv'
+# outFn <- "/Users/youngjm/Data/clip/fs6_stats/precentiles_rawdata_plus_metadata.csv"
+fn <- '/Users/youngjm/Data/clip/fs6_stats/fs6_structural_stats_combatted_covariates_removed_plus_metadata.csv'
+outFn <- "/Users/youngjm/Data/clip/fs6_stats/precentiles_combatted_covariates_removed_plus_metadata.csv"
+analysisDf <- read.csv(fn)
+
+# Convert some columns to factors
+toFactor <- c('sex', 'fs_version', 'MagneticFieldStrength', 'scanner_id', 'scan_reason_primary')
+analysisDf[toFactor] <- lapply(analysisDf[toFactor], factor)
+
+## Step 4: Prep data for centilization ---------
+
+prepForCentilizing <- function(df, fn){
+  toCentilizeCols <- c('scan_id', 'age_in_years', 'sex', 'TotalGrayVol', 
+                       'CerebralWhiteMatterVol', 'SubCortGrayVol', 'VentricleVolume',
+                       'CorticalSurfaceArea', 'MeanCorticalThickness', 'TCV',
+                       'SurfaceHoles', 'scanner_id', 'top_scan_reason_factors')
+  newNames <- c('participant', 'Age', 'sex', 'GMV', 'WMV', 'sGMV', 'Ventricles', 
+                'SA', 'CT', 'TCV',
+                'SurfaceHoles', 'scanner_id', 'top_scan_reason_factors')
+  toCentilizeDf <- df[, toCentilizeCols]
+  toCentilizeDf <- setnames(toCentilizeDf, toCentilizeCols, newNames)
+  
+  toCentilizeDf <- toCentilizeDf %>%
+    mutate(sex = case_when(
+      grepl('F', sex, fixed=TRUE) ~ "Female",
+      grepl('M', sex, fixed=TRUE) ~ "Male"
+    ))
+  
+  toCentilizeDf$age_days <- df$age_at_scan_days + 280 # add gestational age
+  toCentilizeDf$study <-"JMY_CONT_FS6"
+  toCentilizeDf$fs_version <- "FS6_T1"
+  toCentilizeDf$country <- "USA"
+  toCentilizeDf$run <- 1
+  toCentilizeDf$session <- 1
+  toCentilizeDf$dx <- "CN"
+  reorderCols <- c('participant', 'Age', 'age_days', 'sex', 'study', 'fs_version',
+                   'scanner_id', 'country', 'run', 'session', 'dx', 'top_scan_reason_factors',
+                   'SurfaceHoles',
+                   'GMV', 'WMV', 'sGMV', 'Ventricles', 'SA', 'CT', 'TCV')
+  toCentilizeDf <- toCentilizeDf[, reorderCols]
+  toCentilizeDf$INDEX.TYPE <- "NA"
+  toCentilizeDf$INDEX.OB <- "NA"
+  
+  write.csv(toCentilizeDf, fn)
+  return(toCentilizeDf)
+}
+
+preCenDf <- prepForCentilizing(analysisDf, outFn)
